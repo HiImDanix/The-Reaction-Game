f28efb675804a9ea68d04032be2420134c416cb1
Daniels Kanepe <dak@e-komplet.dk>
2024-10-14 22:55:53 +0200

    Update ColorTapConstants.cs

--- Server/Domain/Constants/ColorTapConstants.cs
+++ Server/Domain/Constants/ColorTapConstants.cs
@@ -4,7 +4,7 @@ public static class ColorTapConstants
 {
     public const string Name = "Color Tap";
     public const string Instructions = "Tap when the color matches the word!";
-    public static readonly TimeSpan InstructionsDuration = TimeSpan.FromSeconds(5);
-    public static readonly TimeSpan RoundDuration = TimeSpan.FromSeconds(5);
-    public static readonly TimeSpan WordDisplayDuration = TimeSpan.FromSeconds(1.5);
+    public static readonly TimeSpan InstructionsDuration = TimeSpan.FromSeconds(1);
+    public static readonly TimeSpan DefaultRoundDuration = TimeSpan.FromSeconds(3);
+    public static readonly int DefaultRoundsCount = 3;
 }
